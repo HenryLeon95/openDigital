cfc0fca2a08f4fdbcbbdfe6ab35937b70c06cbda
HenryLeon95 <henriscoh1995@gmail.com>
2020-09-01 12:33:53 -0600

    Creando entorno para PostgreSql en el futuro

--- system/settings.py
+++ system/settings.py
@@ -82,6 +82,19 @@ DATABASES = {
 }
 
 
+#Base de datos para Postgresql
+'''DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hackathon',
+        'USER': 'eduardo',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}'''
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
